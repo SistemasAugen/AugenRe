31ef25f134cab1b46d35491c0743206916fdb8a7
Daniela Ocampo <sistemas@augenlabs.com>
2022-05-26 16:26:36 -0500

    first commit master macbook air sistemas

---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Augen RE</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/main.css?v=<?= time() ?>">
+        <link rel="icon" sizes="192x192" href="https://static.wixstatic.com/media/825d36_30eb68c680d34fb39856bcebfa294503%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/825d36_30eb68c680d34fb39856bcebfa294503%7Emv2.png">
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    </head>
+    <body>
+        <div id="section1" class="section">
+            <div class="row no-gutters">
+                <div class="col-md-12">
+                    <div class="logo">
+                        <img src="assets/images/1.png" alt="logo"/>
+                    </div>
+                    <div class="mark-title">
+                        <h2>Ubica tu óptica.</h2>
+                    </div>
+                </div>
+                <div class="col-md-6 offset-md-1">
+                    <h1>Si en un año cambia tu<br>graduación en las lentes<br>de tus anteojos, ¡no pagues<br>otra vez por unas nuevas!</h1>
+                    <p>Porque esta vez irá por nuestra cuenta.</p>
+                    <p>Y es que en Augen Lab, junto con nuestras ópticas afiliadas, hemos decidido que al llevarte unas lentes Augen, las próximas lentes graduadas serán <b>totalmente gratis.</b></p>
+                    <p><b>Ubica</b> aquí una óptica cercana en la que puedes hacer efectiva esta promoción.</p>
+                    <div class="row no-gutters">
+                        <div class="col-md-6" style="text-align: center;padding-top: 15px;">
+                            <a href="#ubica-tu-optica" class="button">Ubica tu óptica</a>
+                            <!-- <img src="assets/images/4_boton1.png" alt="button-1" class="button-img"> -->
+                        </div>
+                        <div class="col-md-6">
+                            <img src="assets/images/3.png" alt="augen-logo" class="augen-logo">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div style="display: block;">
+                        <img src="assets/images/2.png" alt="model" class="model">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section2">
+            <div class="row no-gutters">
+                <div class="col-md-7 offset-md-1 dotted">
+                    <h2>¿Cómo obtener mis<br/>próximas lentes gratis?</h2>
+                </div>
+                <div class="col-md-4 offset-md-1" style="position: relative; ">
+                    <br/>
+                    <p>Fácil: una vez que hayas localizado tu óptica participante más cercana, acude a ella y pregunta por la promoción de Augen.</p>
+                    <p>En ella te darán tus nuevos anteojos con las lentes Augen y un cupón válido para que, cuando necesites cambiarlas,
+                    lo hagas totalmente gratis.*</p>
+                    <small>* Aplica para todas las lentes antirreflejantes en monofocal, flat top y progresivo,
+que sean elaboradas con Trivex, Trivex 1.60, Trivex Parasol, Parasol, B-block y Polarizados. Incluye paquetes. La puedes hacer efectiva después de 1 año y antes de 2 años a partir de la fecha de tu pedido.</small>
+                </div>
+                <div class="col-md-4 offset-md-1" style="padding-top: 25px;">
+                    <div class="step">
+                        <h3>PASO 1</h3>
+                        <p>Visita tu óptica más cercana. <span><a href="#ubica-tu-optica">Aquí</a> puedes encontrarla.</span></p>
+                    </div>
+                    <div class="step">
+                        <h3>PASO 2</h3>
+                        <p>Pide que las lentes sean Augen.<span>Al hacerlo, el profesional encargado te dará también un cupón por otras totalmente gratis.</span></p>
+                    </div>
+                    <div class="step">
+                        <h3>PASO 3</h3>
+                        <p>Solicita el cupón de tus próximas lentes gratis.<span>Guarda ese cupón, porque lo podrás hacer válido un año después, cuando tu graduación haya cambiado o, incluso, ¡cuando tus lentes se te hayan roto!</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section3" class="section">
+            <div class="container">
+                <h1>Augen Re:</br>
+                    la graduación<br/>
+                    de tus próximas lentes</br>
+                    va por nuestra cuenta.
+                </h1>
+                <p style="margin-bottom: 35px;">Si en un año cambia tu graduación en las lentes<br/>de tus anteojos, ¡no pagues otra vez por unas nuevas!</p>
+                <!-- <img src="assets/images/5_boton2.png" class="button2" alt="button2"> -->
+                <a href="#ubica-tu-optica" class="button">Ubica tu óptica</a>
+                <img src="assets/images/3.png" alt="augen-logo" class="augen-logo">
+            </div>
+        </div>
+        <div id="ubica-tu-optica">
+            <div class="row no-gutters">
+                <div class="col-md-12">
+                    <div class="logo">
+                        <img src="assets/images/9_Re.png" alt="logo"/>
+                    </div>
+                    <div class="mark-title">
+                        <h2>Ubica tu óptica.</h2>
+                    </div>
+                </div>
+                <div class="col-md-10 offset-md-2 geolocator-container">
+                    <div class="row no-gutters">
+                        <!-- <div class="col-md-4 optic-list loading">
+                            <div class="loader">Loading...</div>
+                        </div> -->
+                        <div class="col-md-12 map">
+                            <div id="map"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12" style="text-align: center;">
+                    <img src="assets/images/6.png" alt="augen-logo-alt" class="augen-logo-alt">
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAdFRqF-i8mrYdJryRENhZ25e-P2ym_SBs&callback=initMap&libraries=&v=weekly" defer></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <script type="text/javascript">
+            var markers = [];
+            var infoWindow = null;
+
+            var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+                || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4));
+            console.log(isMobile);
+            $(".smooth-scroll").on("click", function (e) {
+              // 1
+              e.preventDefault();
+              // 2
+              const href = $(this).attr("href");
+              // 3
+              $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+            });
+
+            function initMap() {
+                if ("geolocation" in navigator) {
+                  /* la geolocalización está disponible */
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        console.log(position.coords.latitude, position.coords.longitude);
+                        // alert(position.coords.latitude, position.coords.longitude);
+
+                        map = new google.maps.Map(document.getElementById('map'), {
+                            center: { lat: position.coords.latitude, lng: position.coords.longitude },
+                            zoom: 8
+                        });
+
+                        var noPoi = [{
+                            featureType: "poi",
+                                stylers: [
+                                    { visibility: "off" }
+                                ]
+                            }
+                        ];
+
+                        map.setOptions({ styles: noPoi });
+
+                        let userMarker = new google.maps.Marker({
+                            position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                        });
+
+                        userMarker.setMap(map);
+
+                        map.setCenter(userMarker.getPosition());
+                        map.setZoom(14);
+
+                        $.post('https://sistema.augenlabs.com/api/v1/re', { lat: position.coords.latitude, lng: position.coords.longitude }).then(result => {
+                            // console.log(result);
+
+                            $('.optic-list').removeClass('loading');
+                            $('.optic-list').html('<ul class="real-list"></ul>');
+
+                            var i = 0;
+                            for(let optics of result) {
+                                let mapUrl = `https://www.google.com/maps/dir//${ optics.latitude },${ optics.longitude }/@${ optics.latitude },${ optics.longitude },17z`;
+                                // if(!isMobile)
+                                //     $('.real-list').append(`
+                                //         <li class="item" data-index="${ i }">
+                                //             <h2>${ optics.commercial_name }</h2>
+                                //             <p>${ optics.address } Col. ${ optics.neighborhood }</p>
+                                //             <p>${ optics.schedule }</p>
+                                //             <a href="${ mapUrl }" target="_blank">¿Cómo llegar?</a>
+                                //         </li>
+                                //     `);
+
+                                markers[i] = new google.maps.Marker({
+                                    position: new google.maps.LatLng(optics.latitude, optics.longitude),
+                                    icon: 'https://re.augenlabs.com/assets/images/pin.png',
+                                    idx: i,
+                                    opticsData: optics,
+                                    mapUrl: mapUrl
+                                });
+
+                                markers[i].setMap(map);
+
+                                // https://www.google.com/maps/dir//20.6518388,-103.3647042/@20.6518438,-103.3668929,17z
+                                markers[i].addListener("click", m => {
+                                    let selectedMarker = null;
+                                    let lat = m.latLng.lat();
+                                    let lng = m.latLng.lng();
+
+                                    for(let marker of markers) {
+                                        let coords = marker.getPosition();
+                                        if(coords.lat() == lat && coords.lng() == lng) {
+                                            selectedMarker = marker;
+                                        }
+                                    }
+
+                                    console.log(selectedMarker);
+                                    // window.open(mapUrl, '_blank').focus();
+                                    map.setCenter(selectedMarker.getPosition());
+
+                                    // console.log(marker);
+                                    // if(isMobile) {
+
+                                        if(infoWindow)
+                                            infoWindow.close();
+
+                                        infoWindow = new google.maps.InfoWindow({
+                                            content: `
+                                            <div style="text-align: center;">
+                                                <h3 style="color: rgb(53, 0, 154);">${  selectedMarker.opticsData.commercial_name }</h3>
+                                                <p style="font-size: 11px; text-align: center;">${ selectedMarker.opticsData.address } Col. ${ selectedMarker.opticsData.neighborhood }</p>
+                                                <p style="font-size: 11px;">${ selectedMarker.opticsData.schedule }</p>
+                                                <a href="${ selectedMarker.mapUrl }" target="_blank" style="
+
+    color: rgb(255, 85, 14);
+    font-weight: bold;
+    ">¿Cómo llegar?</a>
+                                            </div>
+                                            `,
+                                        });
+
+                                        infoWindow.open(map, selectedMarker);
+                                    // }
+                                });
+                                i++;
+                            }
+
+
+
+                            // if(!isMobile) {
+                            //     $('.real-list li').click(function() {
+                            //         let idx = $(this).data('index');
+                            //
+                            //         map.setCenter(markers[idx].getPosition());
+                            //         map.setZoom(14);
+                            //     });
+                            // }
+
+
+                        });
+                    });
+                } else {
+                    alert('No pudimos obtener tu geolocalización, es posible que no puedas disfrutar toda la expetiencia');
+                      /* la geolocalización NO está disponible */
+                      map = new google.maps.Map(document.getElementById('map'), {
+                          center: { lat: 20.659698, lng: -103.349609 },
+                          zoom: 8
+                      });
+                }
+            }
+        </script>
+    </body>
+</html>
